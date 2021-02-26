34ec7b5c5209f9d1b0bcdfbdf0e5ede6001f087d
Margaret Ulrich <maggie.ulrich@gmail.com>
2021-02-26 18:47:24 -0500

    Update logic.js

--- leaflet/logic.js
+++ leaflet/logic.js
@@ -42,15 +42,15 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
     // Define different map layers
-    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v11/tiles/256/{z}/{x}/{y}?" +
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?" +
       "access_token=pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xteTA2ZWMwM3lsMnZvNDlyemYwY2NtIn0.X40CcPKvNuPGRNeJd4LPkQ" +
       "T6YbdDixkOBWH_k9GbS8JQ");
 
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v11/tiles/256/{z}/{x}/{y}?" +
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
       "access_token=pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xteTA2ZWMwM3lsMnZvNDlyemYwY2NtIn0.X40CcPKvNuPGRNeJd4LPkQ." +
       "T6YbdDixkOBWH_k9GbS8JQ");
     
-    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}?" +
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?" +
       "access_token=pk.eyJ1IjoibXVscmljaCIsImEiOiJja2xteTA2ZWMwM3lsMnZvNDlyemYwY2NtIn0.X40CcPKvNuPGRNeJd4LPkQ." +
       "T6YbdDixkOBWH_k9GbS8JQ");
   
